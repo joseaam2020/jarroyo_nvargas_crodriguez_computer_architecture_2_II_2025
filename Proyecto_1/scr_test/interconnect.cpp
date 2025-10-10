7a3c975ba5c51cdbbea620343a05115c819879a3
Carolina Rodríguez <gennel7@estudiantec.cr>
2025-10-10 14:39:15 -0600

    Actualizacion del interconnect

--- Proyecto_1/scr_test/interconnect.cpp
+++ Proyecto_1/scr_test/interconnect.cpp
@@ -3,18 +3,20 @@
 
 Interconnect::Interconnect(Memory* mem) : memory(mem) {}
 
+//Recibo los snoops
 void Interconnect::registerSnoopModule(SnoopModule* snoop) {
     snoop_modules.push_back(snoop);
 }
 
+//Señal de lectura desde el snoop
 Interconnect::BusResult Interconnect::broadcastRead(int requesting_pe, int address) {
     std::cout << "\n[BUS] PE" << requesting_pe << " broadcasts READ for address " << address << std::endl;
 
     BusResult result;
-
+//Reviso cada snoop, me devuelve el estado de cada uno.
     for (auto snoop : snoop_modules) {
         auto response = snoop->handleBusRead(address);
-
+//Si es un hit
         if (response.hit) {
             result.shared = true;
             result.data = response.data;
@@ -26,7 +28,7 @@ Interconnect::BusResult Interconnect::broadcastRead(int requesting_pe, int addre
             }
         }
     }
-
+//Si nadie lo tiene voy a memoria a leer el dato
     if (!result.modified && !result.shared) {
         std::cout << "  [BUS] No cache hit, fetching from memory" << std::endl;
         result.data = memory->read(address);
@@ -34,10 +36,11 @@ Interconnect::BusResult Interconnect::broadcastRead(int requesting_pe, int addre
 
     return result;
 }
-
+//En el caso de que sea exclusive
 Interconnect::BusResult Interconnect::broadcastReadX(int requesting_pe, int address) {
     std::cout << "\n[BUS] PE" << requesting_pe << " broadcasts READ_X (exclusive) for address " << address << std::endl;
 
+//Espacio para el estado.
     BusResult result;
 
     for (auto snoop : snoop_modules) {
@@ -60,7 +63,7 @@ Interconnect::BusResult Interconnect::broadcastReadX(int requesting_pe, int addr
 
     return result;
 }
-
+//Notifico a las snoops en ponerse en invalidate
 void Interconnect::broadcastInvalidate(int requesting_pe, int address) {
     std::cout << "\n[BUS] PE" << requesting_pe << " broadcasts INVALIDATE for address " << address << std::endl;
 
